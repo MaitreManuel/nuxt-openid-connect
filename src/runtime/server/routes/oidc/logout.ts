38114e9af0048298e336860c49ae0754d7140174
Trystan EVENO <trystan.eveno@kleegroup.com>
2024-03-06 16:52:34 +0100

    Save work

--- src/runtime/server/routes/oidc/logout.ts
+++ src/runtime/server/routes/oidc/logout.ts
@@ -1,15 +1,29 @@
 import { getCookie, deleteCookie, defineEventHandler } from 'h3'
 import { useRuntimeConfig } from '#imports'
+import { getCallbackUrl, getDefaultBackUrl, getRedirectUrl, getResponseMode } from '../../../utils/utils';
+import { initClient } from '../../../utils/issueclient';
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
+  const req = event.node.req
   const res = event.node.res
   console.log('[LOGOUT]: oidc/logout calling')
 
-  const { config } = useRuntimeConfig().openidConnect
+  const { op, config } = useRuntimeConfig().openidConnect
+  const redirectUrl = getRedirectUrl(req.url)
+  const defCallBackUrl = getDefaultBackUrl(redirectUrl, req.headers.host)
+
   deleteCookie(event, config.secret)
   deleteCookie(event, config.cookiePrefix + 'access_token')
   deleteCookie(event, config.cookiePrefix + 'refresh_token')
   deleteCookie(event, config.cookiePrefix + 'user_info')
+
+  const issueClient = await initClient(op, req, [defCallBackUrl])
+
+  const parameters = {
+    post_logout_redirect_uri: defCallBackUrl,
+  }
+  const logoutUrl = issueClient.endSessionUrl(parameters)
+  console.log('[Logout]: Logout Url: ' + logoutUrl)
 
   // delete part of cookie userinfo (depends on user's setting.).
   const cookie = config.cookie
@@ -19,6 +33,6 @@ export default defineEventHandler((event) => {
     }
   }
 
-  res.writeHead(302, { Location: '/' })
+  res.writeHead(302, { Location: logoutUrl })
   res.end()
 })
